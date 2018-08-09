f1734728876e2c6b8a3640371f86a2149e80b487
christinaalenda <clenda041@gmail.com>
2018-08-09 18:59:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,7 @@
 # dog.rb
 class Dog
-  def initialize(name)
-end
-def name
-  @name = fido
 end
 
-def name=(new.Dog)
-  @name = new.Dog
-end
-
-end
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
